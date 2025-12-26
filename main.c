60d321468c80c933386dd93b7f61dbc08bda6050
dweg0o <139035174+dweg0o@users.noreply.github.com>
2025-12-26 14:21:05 -0300

    argv has been implemented

--- main.c
+++ main.c
@@ -1,43 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dna_rna/dna_rna.h"
 #include "rna_complement/rna_complement.h"
 #include "rna_identification/rna_identification.h"
 #include "rna_aminoacids/genetic_code.h"
 
-int main() {
-    char dnaSequence[] = "ATGCTAGCTA";
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s <DNA_SEQUENCE>\n", argv[0]);
+        return 1;
+    }
+
+    const char *dnaSequence = argv[1];
+
     printf("Project 1: DNA to RNA\n");
     printf("DNA sequence: %s\n", dnaSequence);
 
     char *rnaSequence = dnaToRna(dnaSequence);
+    if (!rnaSequence) {
+        printf("Error converting DNA to RNA.\n");
+        return 1;
+    }
+
     printf("RNA sequence: %s\n\n", rnaSequence);
-    free(rnaSequence);
 
-    char rnaComplementSequence[] = "AUGCUAGCUA";
     printf("Project 2: RNA Complement\n");
-    printf("RNA sequence: %s\n", rnaComplementSequence);
+    char *complement = complementRna(rnaSequence);
+    if (!complement) {
+        printf("Error generating RNA complement.\n");
+        free(rnaSequence);
+        return 1;
+    }
 
-    char *complement = complementRna(rnaComplementSequence);
     printf("RNA complement: %s\n\n", complement);
-    free(complement);
 
-    char rnaIdentificationSequence[] = "AUGCUAGCUAUAG";
     printf("Project 3: RNA Identification\n");
-    printf("RNA sequence: %s\n", rnaIdentificationSequence);
-
-    char identification = identifyRna(rnaIdentificationSequence);
+    char identification = identifyRna(rnaSequence);
     printf("RNA identification: %c\n\n", identification);
+
+    printf("Project 4: RNA to Amino Acids Translation\n");
 
     struct GeneticCodeEntry code[64];
     initGeneticCode(code);
 
-    const char *rnaTranslationSequence = "AUGUUUUUCUGU";
-    printf("Project 4: RNA to Amino Acids Translation\n");
-    printf("RNA sequence: %s\n", rnaTranslationSequence);
+    char *aminoAcids = rnaToAminoAcids(rnaSequence, code);
+    if (!aminoAcids) {
+        printf("Error translating RNA to amino acids.\n");
+        free(rnaSequence);
+        free(complement);
+        return 1;
+    }
 
-    char *aminoAcids = rnaToAminoAcids(rnaTranslationSequence, code);
     printf("Amino acids sequence: %s\n", aminoAcids);
+
+    free(rnaSequence);
+    free(complement);
     free(aminoAcids);
 
     return 0;
